320257c0b57e67043bc78139d7e0b80532ecdf05
Stackingrule <messageswitching@gmail.com>
2019-12-25 22:29:40 +0800

    start project

---
+++ src/main/java/com/stackingrule/dianping/DianpingApplication.java
@@ -0,0 +1,13 @@
+package com.stackingrule.dianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianpingApplication.class, args);
+    }
+
+}
