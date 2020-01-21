8c188d3ada3a9e40dd9799dd5fcfabc1b2d71842
Stackingrule <messageswitching@gmail.com>
2020-01-21 22:52:26 +0800

    fixed gender

--- src/main/java/com/stackingrule/dianping/controller/UserController.java
+++ src/main/java/com/stackingrule/dianping/controller/UserController.java
@@ -2,16 +2,22 @@ package com.stackingrule.dianping.controller;
 
 import com.stackingrule.dianping.common.*;
 import com.stackingrule.dianping.model.UserModel;
+import com.stackingrule.dianping.request.LoginReq;
 import com.stackingrule.dianping.request.RegisterReq;
 import com.stackingrule.dianping.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 
 
 /*
@@ -22,8 +28,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/user")
 public class UserController {
 
+    public static final String CURRENT_USER_SESSION = "currrentUserSession";
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
 
     @RequestMapping("/test")
     @ResponseBody
@@ -31,7 +42,7 @@ public class UserController {
         return "test";
     }
 
-    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    @RequestMapping("/index")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("/index.html");
         return modelAndView;
@@ -66,6 +77,18 @@ public class UserController {
         UserModel resUserModel = userService.register(registerUser);
 
         return CommonRes.create(resUserModel);
+    }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public CommonRes login(@RequestBody @Valid LoginReq loginReq,BindingResult bindingResult) throws BusinessException, UnsupportedEncodingException, NoSuchAlgorithmException {
+        if(bindingResult.hasErrors()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,CommonUtil.processErrorString(bindingResult));
+        }
+        UserModel userModel = userService.login(loginReq.getTelphone(),loginReq.getPassword());
+        httpServletRequest.getSession().setAttribute(CURRENT_USER_SESSION,userModel);
+
+        return CommonRes.create(userModel);
     }
 
 
--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `dianpingdb`.`Untitled`  (
-  `id` int(0) NOT NULL,
-  `created_at` datetime(255) NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` datetime(0) NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `telphone` varchar(40) NOT NULL DEFAULT '',
-  `password` varchar(200) NOT NULL DEFAULT '',
-  `nick_name` varchar(40) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `telphone_nuique_index`(`telphone`) USING BTREE
-);
+CREATE TABLE `dianpingdb`.`user` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`telphone` varchar(40) NOT NULL DEFAULT '',
+	`password` varchar(200) NOT NULL DEFAULT '',
+	`nick_name` varchar(40) NOT NULL DEFAULT '',
+	`gender` int NOT NULL DEFAULT 0,
+	PRIMARY KEY (`id`),
+	UNIQUE `telphone_unique_index` USING BTREE (`telphone`) comment ''
+) COMMENT='';
