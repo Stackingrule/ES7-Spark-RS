775a3b329e5e1529d68eea31dc398823099d85e3
Stackingrule <messageswitching@gmail.com>
2020-01-29 13:25:34 +0800

    start search v1.0

--- src/main/java/com/stackingrule/dianping/controller/ShopController.java
+++ src/main/java/com/stackingrule/dianping/controller/ShopController.java
@@ -1,6 +1,12 @@
 package com.stackingrule.dianping.controller;
 
 
+import com.stackingrule.dianping.common.BusinessException;
+import com.stackingrule.dianping.common.CommonRes;
+import com.stackingrule.dianping.common.EmBusinessError;
+import com.stackingrule.dianping.model.ShopModel;
+import com.stackingrule.dianping.service.CategoryService;
+import com.stackingrule.dianping.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -13,7 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
+
 @Controller("/shop")
 @RequestMapping("/shop")
 public class ShopController {
@@ -24,46 +30,17 @@ public class ShopController {
     @Autowired
     private CategoryService categoryService;
 
-    //推荐服务V1.0
+    // 推荐服务 V1.0
     @RequestMapping("/recommend")
     @ResponseBody
-    public CommonRes recommend(@RequestParam(name="longitude")BigDecimal longitude,
-                               @RequestParam(name="latitude")BigDecimal latitude) throws BusinessException {
-        if(longitude == null || latitude == null){
+    public CommonRes recommend(@RequestParam(name = "longitude") BigDecimal longitude, @RequestParam(name = "latitude") BigDecimal latitude) throws BusinessException {
+        if (longitude == null || latitude == null) {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
         }
 
-        List<ShopModel> shopModelList = shopService.recommend(longitude,latitude);
+        List<ShopModel> shopModelList = shopService.recommend(longitude, latitude);
         return CommonRes.create(shopModelList);
     }
 
 
-    //搜索服务V1.0
-    @RequestMapping("/search")
-    @ResponseBody
-    public CommonRes search(@RequestParam(name="longitude")BigDecimal longitude,
-                            @RequestParam(name="latitude")BigDecimal latitude,
-                            @RequestParam(name="keyword")String keyword,
-                            @RequestParam(name="orderby",required = false)Integer orderby,
-                            @RequestParam(name="categoryId",required = false)Integer categoryId,
-                            @RequestParam(name="tags",required = false)String tags) throws BusinessException {
-        if(StringUtils.isEmpty(keyword) || longitude == null || latitude == null){
-            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR);
-        }
-
-        List<ShopModel> shopModelList = shopService.search(longitude,latitude,keyword,orderby,categoryId,tags);
-        List<CategoryModel> categoryModelList = categoryService.selectAll();
-        List<Map<String,Object>> tagsAggregation = shopService.searchGroupByTags(keyword,categoryId,tags);
-        Map<String,Object> resMap = new HashMap<>();
-        resMap.put("shop",shopModelList);
-        resMap.put("category",categoryModelList);
-        resMap.put("tags",tagsAggregation);
-        return CommonRes.create(resMap);
-
-    }
-
-
-
-
 }
- */
